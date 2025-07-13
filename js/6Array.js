4af7bf45afd1c8f0d6475a28d04072233168eb17
ahkamul <ahkamulbd@gmail.com>
2025-07-13 17:05:46 +0600

    Chapter 6 i.e. array problem solved

---
+++ js/6Array.js
@@ -0,0 +1,140 @@
+/** Problem 1 from Page # 93 */
+
+const yourArray = [71, 72, 73, 74, 75, 76, 77, 78, 79];
+
+/** Problem 2 from Page # 93 */
+
+const fruitsInFreeze = ['Apple', 'Banana', 'Orange', 'Mango', 'Guava'];
+
+/** Problem 3 from Page # 93 */
+
+const vegitableInBag = ['Potato', 'Onion', 'Brinzol', 'Radish', 'Carrot', 'Tomato', 'Dedosh', 'Laow', 'Sosha', 'Potal'];
+const numberOfVegitables = vegitableInBag.length;
+
+// console.log(numberOfVegitables);
+
+
+/** ==============================================================================================
+ *  ==============================================================================================
+ */
+
+/** Problem 1 from Page # 96 */
+
+const yourFriends = ['Tapu', 'Apu', 'Shilo', 'Balu', 'Kalu', 'Shalo'];
+// console.log(yourFriends[3]);
+
+/** Problem 2 from Page # 96 */
+
+const favouriteBooks = ['Asomapto Attmojiboni', 'Gitangoli', 'Sonartori', 'Sangsaptok', 'Gavi Bittanto', 'Zoddopi Amar Guru'];
+// console.log(favouriteBooks[5]);
+
+/** Problem 3 from Page # 96 */
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+numbers[7] = 30;
+// console.log(numbers);
+
+/** Problem 4 from Page # 96 */
+
+// const mobileGames = ['Free Fire', 'Pubgi', 'Kandi Crush', 'Temple Run'];
+// mobileGames[2] = 'Subway Surfer';
+// console.log(mobileGames);
+
+/** Following number 5 to 7 from Page # 96 */
+
+/** =======================================================================
+ *  =======================================================================
+ */
+
+/** Problem 1 from Page # 99 */
+
+const numbersArray = [10, 20, 30, 40, 50];
+numbersArray.push(60);
+
+// console.log(numbersArray);
+
+/** Problem 2 from Page # 99 */
+
+const friends = ['Sajib', 'Sagor', 'Sakib', 'Sohel'];
+friends.push('Sumon');
+
+// console.log(friends);
+
+/** Problem 3 from Page # 99 */
+
+const mobileGames = ['Free Fire', 'Pubgi', 'Kandi Crush', 'Temple Run'];
+mobileGames.pop();
+
+// console.log(mobileGames);
+
+/** Problem 4 from Page # 100 */
+
+const arrayNumbers = [24, 36, 48, 60];
+arrayNumbers.unshift(12);
+
+// console.log(arrayNumbers);
+
+/** Problem 5 from Page # 100 */
+
+const myBooks = ['Asomapto Attmojiboni', 'Gitangoli', 'Sonartori', 'Sangsaptok', 'Gavi Bittanto', 'Zoddopi Amar Guru'];;
+myBooks.shift();
+
+// console.log(myBooks);
+
+/** =================================================================================================
+ *  =================================================================================================
+ */
+
+/** Problem 1 from Page # 102 */
+
+const fruitsArray = ['Apple', 'Banana', 'Mango', 'Licci'];
+
+if (fruitsArray.includes('Mango')){
+    // console.log('Mango exists');
+}
+else{
+    // console.log('Mango not exists, climb on the tree');
+}
+
+/** Problem 2 from Page # 102 */
+
+const names = ['Babul', 'Alif', 'Choton'];
+
+const babulIndex = names.indexOf('Babul');
+
+// console.log(babulIndex);
+
+/** Problem 3 from Page # 102 */
+
+const friendNames = ['Rimon', 'Rifat', 'Rajib'];
+
+const rifatIndex = friendNames.indexOf('Rifat');
+
+// console.log(rifatIndex);
+
+/** Problem 4 from Page # 102 */
+
+const cities = ['Dhaka', 'Chittagong', 'Sylhet'];
+
+cities.push('Rajshahi')
+
+// console.log(cities.includes('Rajshahi'));
+
+/** Problem 5 from Page # 102 */
+
+const eliments = ['Ponds', 'Cludes', 'Rain', 'Rainy Season'];
+
+if (eliments.includes('Rain')){
+    // console.log('I need umberalla');
+}
+else{
+    // console.log('No rain, no pain');
+}
+
+/** Problem 6 from Page # 102 */
+
+const games = ['Football', 'Cricketd', 'Volleyball'];
+
+console.log(games.indexOf('Badminton'));
+
+
